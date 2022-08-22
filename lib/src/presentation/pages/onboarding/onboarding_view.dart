619c023466837cc161c781528968f0a627cc8e38
thanhphamtrung <thanhpt22emu@gmail.com>
2022-08-23 00:50:50 +0700

    add onboarding page UI

--- lib/src/presentation/pages/onboarding/onboarding_view.dart
+++ lib/src/presentation/pages/onboarding/onboarding_view.dart
@@ -7,35 +7,65 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
-        horizontal: 15.0,
+        horizontal: 16.0,
         vertical: 50.0,
       ),
       child: Column(
         children: [
-          Text(
-            'Animation',
-            style: Theme.of(context)
-                .textTheme
-                .headline4
-                ?.copyWith(fontWeight: FontWeight.bold, color: Colors.black),
+          Expanded(
+            flex: 4,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Animation',
+                style: Theme.of(context).textTheme.headline4?.copyWith(
+                    fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
           ),
-          Text(
-            'eCommerce mobile app',
-            style: Theme.of(context).textTheme.subtitle2,
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text(
+                'eCommerce mobile app',
+                style: Theme.of(context).textTheme.subtitle2,
+              ),
+            ),
           ),
-          Image.asset('assets/images/intro-bg.png'),
-          Text(
-            'A one-stop online shop for all of your needs. Buy the latest products from popular brands!',
-            style: Theme.of(context).textTheme.bodyText2,
+          Expanded(
+            flex: 7,
+            child: Image.asset('assets/images/intro-bg.png'),
           ),
-          FloatingActionButton(
-            onPressed: () {},
-            child: const Icon(Icons.arrow_forward_sharp),
+          Expanded(
+            flex: 4,
+            child: Center(
+              child: Text(
+                'A one-stop online shop for all of your needs. Buy the latest products from popular brands!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ),
           ),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Skip'),
-          )
+          Expanded(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.arrow_forward_sharp),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text('Skip'),
+              ),
+            ),
+          ),
         ],
       ),
     );
