12e429b7eec2e45177fc6061836a0c80d51f843e
thanhphamtrung <thanhpt22emu@gmail.com>
2022-08-23 00:29:14 +0700

    update theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -13,7 +13,26 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'Josefin'),
+      theme: ThemeData(
+        fontFamily: 'Josefin',
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Color(0xFFfc5130),
+            foregroundColor: Colors.white,
+            iconSize: 16.0),
+        colorScheme: const ColorScheme(
+          primary: Color(0xFF171717),
+          secondary: Color(0x0064d2ff),
+          surface: Colors.purpleAccent,
+          background: Color(0xFFFFFFFF),
+          error: Color(0xFFFFFFFF),
+          onPrimary: Colors.red,
+          onSecondary: Colors.deepOrange,
+          onSurface: Color(0xFFFFFFFF),
+          onBackground: Color(0xFFFFFFFF),
+          onError: Colors.redAccent,
+          brightness: Brightness.light,
+        ),
+      ),
       home: const SafeArea(
         child: Scaffold(
           body: OnboardingPage(),
--- lib/src/presentation/pages/onboarding/onboarding_view.dart
+++ lib/src/presentation/pages/onboarding/onboarding_view.dart
@@ -5,22 +5,39 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Animation'),
-        const Text('eCommerce mobile app'),
-        Image.asset('assets/images/intro-bg.png'),
-        const Text(
-            'A one-stop online shop for all of your needs. Buy the latest products from popular brands!'),
-        FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.arrow_forward_sharp),
-        ),
-        TextButton(
-          onPressed: () {},
-          child: const Text('Skip'),
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15.0,
+        vertical: 50.0,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Animation',
+            style: Theme.of(context)
+                .textTheme
+                .headline4
+                ?.copyWith(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          Text(
+            'eCommerce mobile app',
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+          Image.asset('assets/images/intro-bg.png'),
+          Text(
+            'A one-stop online shop for all of your needs. Buy the latest products from popular brands!',
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.arrow_forward_sharp),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Skip'),
+          )
+        ],
+      ),
     );
   }
 }
